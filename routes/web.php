bbbff87da1cc99eeba0eafa982431b04d5028c07
Gulshat <159312588+GulshatNasibullina@users.noreply.github.com>
2024-10-04 21:29:02 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route; 
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/product', [ProductController::class, 'index']);
 Route::get('/product/{id}', [ProductController::class, 'show'])->name('show');
 Route::post('/order', [OrderController::class, 'store'])->name('order.store');
