bd3ae4b26a36ee1234652913c5723c7cc2570b8a
Gulshat <159312588+GulshatNasibullina@users.noreply.github.com>
2024-10-06 22:13:03 +0300

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $product->name }}</title>
+    <title>Товары</title>
 </head>
 <body>
     <h1>{{ $product->name }}</h1>
